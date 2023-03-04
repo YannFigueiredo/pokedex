56ff5cacef124622585b24fadca05c1018159443
Yann Figueiredo <yann.fabricio@hotmail.com>
2023-03-04 10:37:50 -0300

    feat: added pokemon type component

---
+++ src/components/PokemonType/index.js
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+import "./styles.css";
+
+export default function PokemonType({ pokemonType }) {
+    const [colorClass, setColorClass] = useState();
+
+    useEffect(() => {
+        switch (pokemonType) {
+            case "bug":
+                setColorClass("bug");
+                break;
+            case "dark":
+                setColorClass("dark");
+                break;
+            case "dragon":
+                setColorClass("dragon");
+                break;
+            case "electric":
+                setColorClass("electric");
+                break;
+            case "fairy":
+                setColorClass("fairy");
+                break;
+            case "fighting":
+                setColorClass("fighting");
+                break;
+            case "fire":
+                setColorClass("fire");
+                break;
+            case "flying":
+                setColorClass("flying");
+                break;
+            case "ghost":
+                setColorClass("ghost");
+                break;
+            case "normal":
+                setColorClass("normal");
+                break;
+            case "grass":
+                setColorClass("grass");
+                break;
+            case "ground":
+                setColorClass("ground");
+                break;
+            case "ice":
+                setColorClass("ice");
+                break;
+            case "poison":
+                setColorClass("poison");
+                break;
+            case "psychic":
+                setColorClass("psychic");
+                break;
+            case "rock":
+                setColorClass("rock");
+                break;
+            case "steel":
+                setColorClass("steel");
+                break;
+            case "water":
+                setColorClass("water");
+                break;
+            default:
+                break;
+        }
+    }, []);
+
+    return (
+        <div className="type">
+            <span className={`type-text ${colorClass}`}></span>
+        </div>
+    );
+}
